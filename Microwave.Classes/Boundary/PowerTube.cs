8442460ab584e99a71dcce9486b374054459a87e
FrederikLaursenSW <60841888+FrederikLaursenSW@users.noreply.github.com>
2022-04-26 17:42:55 +0200

    Powertube implement Changed

--- Microwave.Classes/Boundary/PowerTube.cs
+++ Microwave.Classes/Boundary/PowerTube.cs
@@ -9,16 +9,32 @@ namespace Microwave.Classes.Boundary
 
         private bool IsOn = false;
 
-        public PowerTube(IOutput output)
+        public enum PowerSupplyEnum {
+            watt1000 = 1000,
+            watt800 = 800,
+            watt700 = 700,
+            watt500 = 500
+        }
+
+        public PowerSupplyEnum PowerSupply { get;}
+
+        public PowerTube(IOutput output, PowerSupplyEnum powerSupply)
         {
             myOutput = output;
+            PowerSupply = powerSupply;
+            
+        }
+
+        public int GetMaxPower()
+        {
+            return (int)PowerSupply;
         }
 
         public void TurnOn(int power)
         {
-            if (power < 1 || 700 < power)
+            if (power < 1 || ((int)PowerSupply) < power)
             {
-                throw new ArgumentOutOfRangeException("power", power, "Must be between 1 and 700 (incl.)");
+                throw new ArgumentOutOfRangeException("power", power, "Must be between 1 and" + PowerSupply.ToString() + "(incl.)");
             }
 
             if (IsOn)
